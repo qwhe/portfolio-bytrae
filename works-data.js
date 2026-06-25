79ebb836e2a09edebcbd57a1ecf73b64ceb08ef7
qwhe <qwhe@foxmail.com>
2026-06-25 11:50:47 +0000

    Major redesign: particle hero, featured carousel, masonry grid, multi-image detail pages
    - Hero: Canvas particle system, letter bounce animation, typewriter effect, parallax, custom cursor
    - Featured Works: Full-width horizontal carousel with 3D tilt, auto-play, drag support
    - All Works: Masonry layout with stagger animations, hover effects
    - 19 detail pages: Multi-image gallery with alternating layout, project info, prev/next navigation
    - 111 real work images downloaded from ZCOOL
    - 20 works with complete data (titles, descriptions, tags, view counts)

---
+++ works-data.js
@@ -0,0 +1,489 @@
+const WORKS = [
+  {
+    "slug": "feilong-duanwu",
+    "title": "飞龙在天·利见大人 百度端午高端礼盒包装",
+    "views": 13279,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "百度",
+      "端午",
+      "星相"
+    ],
+    "description": "创意输出：山魈山+百度BMD\n设计执行：山魈山+qwhe+百度BMD\n插图执行：奇舫社\n摄影执行：锐图摄影\n设计出品：百度BMD\n最近打算整理一下案例",
+    "cover": "images/works/feilong-duanwu/feilong-duanwu-cover.jpg",
+    "coverW": 1280,
+    "coverH": 10393,
+    "detailImages": [
+      "images/works/feilong-duanwu/feilong-duanwu-01.jpg",
+      "images/works/feilong-duanwu/feilong-duanwu-02.jpg",
+      "images/works/feilong-duanwu/feilong-duanwu-03.jpg",
+      "images/works/feilong-duanwu/feilong-duanwu-04.jpg",
+      "images/works/feilong-duanwu/feilong-duanwu-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "baidu-duanwu-gift",
+    "title": "百度端午员工礼盒",
+    "views": 19702,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "百度",
+      "端午",
+      "礼品"
+    ],
+    "description": "今年的百度直销端午福利礼盒，好吃好看又好玩~\n是各位熊孩子的必备佳品！\n除了肯粽子躲清凉，还可以跟小度熊一起~\n开启一场充满奇幻色彩的端午奇遇记......\n感谢摄影：@一如初见",
+    "cover": "images/works/baidu-duanwu-gift/baidu-duanwu-gift-cover.jpg",
+    "coverW": 1280,
+    "coverH": 800,
+    "detailImages": [
+      "images/works/baidu-duanwu-gift/baidu-duanwu-gift-01.jpg",
+      "images/works/baidu-duanwu-gift/baidu-duanwu-gift-02.jpg",
+      "images/works/baidu-duanwu-gift/baidu-duanwu-gift-03.jpg",
+      "images/works/baidu-duanwu-gift/baidu-duanwu-gift-04.jpg",
+      "images/works/baidu-duanwu-gift/baidu-duanwu-gift-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "baidu-beifang-zhongqiu",
+    "title": "百度北分中秋礼品",
+    "views": 39269,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "中秋",
+      "百度",
+      "月球"
+    ],
+    "description": "\"AI\" want to see you~\n历时2个月，这套百度北分的2018中秋礼品终于做完啦~~\n需求方：百度北分公司\n设计：Jero\n插画：艺洋\n摄影：@一如初见\n出品：蓝桃文化\n产品：月球灯，月饼，光感车，桌垫，金属书签",
+    "cover": "",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [],
+    "imageCount": 0
+  },
+  {
+    "slug": "baidu-xingxing-manghe",
+    "title": "百度中秋礼盒 中国行星探测联名度熊探测系列盲盒",
+    "views": 7195,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "百度",
+      "中秋",
+      "度熊"
+    ],
+    "description": "百度2020中秋礼盒 中国行星探测联名度熊探测系列盲盒\n创意输出：山魈山+百度BMD\n设计执行：qwhe+山魈山\n设计出品：百度BMD",
+    "cover": "images/works/baidu-xingxing-manghe/baidu-xingxing-manghe-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/baidu-xingxing-manghe/baidu-xingxing-manghe-01.jpg",
+      "images/works/baidu-xingxing-manghe/baidu-xingxing-manghe-02.jpg",
+      "images/works/baidu-xingxing-manghe/baidu-xingxing-manghe-03.jpg",
+      "images/works/baidu-xingxing-manghe/baidu-xingxing-manghe-04.jpg",
+      "images/works/baidu-xingxing-manghe/baidu-xingxing-manghe-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "ant-financial",
+    "title": "蚂蚁金服商务创意摆件",
+    "views": 999,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "蚂蚁金服",
+      "摆件",
+      "支付"
+    ],
+    "description": "",
+    "cover": "images/works/ant-financial/ant-financial-cover.jpg",
+    "coverW": 1280,
+    "coverH": 615,
+    "detailImages": [
+      "images/works/ant-financial/ant-financial-01.jpg",
+      "images/works/ant-financial/ant-financial-02.jpg",
+      "images/works/ant-financial/ant-financial-03.jpg",
+      "images/works/ant-financial/ant-financial-04.jpg",
+      "images/works/ant-financial/ant-financial-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "baidu-lipindian-zhongqiu",
+    "title": "百度礼品店中秋礼品",
+    "views": 13705,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "百度",
+      "中秋",
+      "礼品"
+    ],
+    "description": "又是一款中秋（捂脸笑。。）\\n这款是百度礼品店售卖用的，\\n考虑做的轻松好玩一些，\\n包装设计采用方形火箭造型~\\n两边两个翅膀可以卡在火箭上立起来做摆拍用~\\n还做了一些模切可以增加趣味性~\\n其实可做的设计很多，不过同期有好几个项目在运行，\\n时间关系就只能留下遗憾下次改进了~\\n这三款中秋做完之后得到的经验就是~\\n很多东西都能复用~哈哈~\\n不光是产品，设计元素和素材都要经常积累才能提高效率~\\n最后祝中秋愉快~\\n设计：艺洋\\n插画：艺洋\\n效果图：刘叶楠\\n摄影：@一如初见\\n出品：蓝桃文化",
+    "cover": "images/works/baidu-lipindian-zhongqiu/baidu-lipindian-zhongqiu-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/baidu-lipindian-zhongqiu/baidu-lipindian-zhongqiu-01.jpg",
+      "images/works/baidu-lipindian-zhongqiu/baidu-lipindian-zhongqiu-02.jpg",
+      "images/works/baidu-lipindian-zhongqiu/baidu-lipindian-zhongqiu-03.jpg",
+      "images/works/baidu-lipindian-zhongqiu/baidu-lipindian-zhongqiu-04.jpg",
+      "images/works/baidu-lipindian-zhongqiu/baidu-lipindian-zhongqiu-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "baidu-newyear",
+    "title": "百度新年礼包",
+    "views": 45357,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "新年",
+      "红包",
+      "对联"
+    ],
+    "description": "",
+    "cover": "images/works/baidu-newyear/baidu-newyear-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/baidu-newyear/baidu-newyear-01.jpg",
+      "images/works/baidu-newyear/baidu-newyear-02.jpg",
+      "images/works/baidu-newyear/baidu-newyear-03.jpg",
+      "images/works/baidu-newyear/baidu-newyear-04.jpg",
+      "images/works/baidu-newyear/baidu-newyear-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "baidu-zhixiao-duanwu",
+    "title": "2019百度直销端午员工礼品",
+    "views": 11714,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "百度",
+      "端午",
+      "礼盒"
+    ],
+    "description": "又是一波端午礼品来到~\\n今年的创意结合是功夫和DU熊，\\n午林萌主，谁为正粽！\\n需求方：百度北分公司\\n设计：Jero\\n插画：艺洋\\n摄影：@一如初见\\n出品：蓝桃文化\\n每一个图案对应一个产品。",
+    "cover": "images/works/baidu-zhixiao-duanwu/baidu-zhixiao-duanwu-cover.jpg",
+    "coverW": 1280,
+    "coverH": 800,
+    "detailImages": [
+      "images/works/baidu-zhixiao-duanwu/baidu-zhixiao-duanwu-01.jpg",
+      "images/works/baidu-zhixiao-duanwu/baidu-zhixiao-duanwu-02.jpg",
+      "images/works/baidu-zhixiao-duanwu/baidu-zhixiao-duanwu-03.jpg",
+      "images/works/baidu-zhixiao-duanwu/baidu-zhixiao-duanwu-04.jpg",
+      "images/works/baidu-zhixiao-duanwu/baidu-zhixiao-duanwu-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "bytedance-duanwu",
+    "title": "字节范端午主题衍生品",
+    "views": 2179,
+    "tags": [
+      "原创Works",
+      "平面",
+      "品牌",
+      "字节跳动",
+      "字节范",
+      "端午"
+    ],
+    "description": "",
+    "cover": "images/works/bytedance-duanwu/bytedance-duanwu-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/bytedance-duanwu/bytedance-duanwu-01.jpg",
+      "images/works/bytedance-duanwu/bytedance-duanwu-02.jpg",
+      "images/works/bytedance-duanwu/bytedance-duanwu-03.jpg",
+      "images/works/bytedance-duanwu/bytedance-duanwu-04.jpg",
+      "images/works/bytedance-duanwu/bytedance-duanwu-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "didi-incense",
+    "title": "滴滴香插礼品设计",
+    "views": 11510,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "香插",
+      "滴滴",
+      "线香"
+    ],
+    "description": "给滴滴定制的高端礼品，红铜材质，结合滴滴logo设计的香插。\\n设计：Jero\\n包装：刘叶楠\\nLOGO 可旋转\\n香台\\n封套\\n包装设计\\n硫酸纸图案",
+    "cover": "images/works/didi-incense/didi-incense-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/didi-incense/didi-incense-01.jpg",
+      "images/works/didi-incense/didi-incense-02.jpg",
+      "images/works/didi-incense/didi-incense-03.jpg",
+      "images/works/didi-incense/didi-incense-04.jpg",
+      "images/works/didi-incense/didi-incense-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "duxiaoman-zhongqiu",
+    "title": "度小满金融中秋员工礼品",
+    "views": 37715,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "度小满金融",
+      "百度",
+      "中秋"
+    ],
+    "description": "这个方案其实去年就已经有了，\\n只是当时客户还没有相对独立出来，\\n诸多原因都给搁置了，\\n今年重新修改了下~\\n传说中的一稿过哈哈~\\n需求方：度小满金融\\n设计：Jero\\n插画：文萍\\n摄影：@一如初见\\n出品：蓝桃文化\\n接下来是大货拍摄了，感觉还原的还可以嘿嘿~\\n蓝色这套客户没采用，但是我很喜欢，所以也放上来了~",
+    "cover": "images/works/duxiaoman-zhongqiu/duxiaoman-zhongqiu-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/duxiaoman-zhongqiu/duxiaoman-zhongqiu-01.jpg",
+      "images/works/duxiaoman-zhongqiu/duxiaoman-zhongqiu-02.jpg",
+      "images/works/duxiaoman-zhongqiu/duxiaoman-zhongqiu-03.jpg",
+      "images/works/duxiaoman-zhongqiu/duxiaoman-zhongqiu-04.jpg",
+      "images/works/duxiaoman-zhongqiu/duxiaoman-zhongqiu-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "qianye-jewelry-h5",
+    "title": "千叶珠宝H5",
+    "views": 451,
+    "tags": [
+      "原创Works",
+      "UI",
+      "交互/UE",
+      "H5",
+      "千叶",
+      "千叶珠宝"
+    ],
+    "description": "一个珠宝品牌的营销H5\\n其实感觉白色更加符合品牌调性，不过客户最后选了黑色",
+    "cover": "images/works/qianye-jewelry-h5/qianye-jewelry-h5-cover.jpg",
+    "coverW": 352,
+    "coverH": 599,
+    "detailImages": [
+      "images/works/qianye-jewelry-h5/qianye-jewelry-h5-01.jpg",
+      "images/works/qianye-jewelry-h5/qianye-jewelry-h5-02.jpg",
+      "images/works/qianye-jewelry-h5/qianye-jewelry-h5-03.jpg",
+      "images/works/qianye-jewelry-h5/qianye-jewelry-h5-04.jpg",
+      "images/works/qianye-jewelry-h5/qianye-jewelry-h5-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "resume-2020",
+    "title": "简历 2020 个人作品整理",
+    "views": 697,
+    "tags": [
+      "原创Works",
+      "平面",
+      "其他平面",
+      "礼品",
+      "礼盒",
+      "节日"
+    ],
+    "description": "最近整理了一下之前做的，好多好乱好繁杂。。。",
+    "cover": "images/works/resume-2020/resume-2020-cover.jpg",
+    "coverW": 1280,
+    "coverH": 9009,
+    "detailImages": [
+      "images/works/resume-2020/resume-2020-01.jpg",
+      "images/works/resume-2020/resume-2020-02.jpg",
+      "images/works/resume-2020/resume-2020-03.jpg",
+      "images/works/resume-2020/resume-2020-04.jpg"
+    ],
+    "imageCount": 5
+  },
+  {
+    "slug": "sogou-translate",
+    "title": "搜狗翻译宝礼盒",
+    "views": 4967,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "搜狗",
+      "翻译宝",
+      "礼盒"
+    ],
+    "description": "",
+    "cover": "images/works/sogou-translate/sogou-translate-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/sogou-translate/sogou-translate-01.jpg",
+      "images/works/sogou-translate/sogou-translate-02.jpg",
+      "images/works/sogou-translate/sogou-translate-03.jpg",
+      "images/works/sogou-translate/sogou-translate-04.jpg",
+      "images/works/sogou-translate/sogou-translate-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "sohu-calendar",
+    "title": "搜狐2016台历",
+    "views": 2152,
+    "tags": [
+      "原创Works",
+      "平面",
+      "宣传物料",
+      "台历",
+      "搜狐台历",
+      "跑步"
+    ],
+    "description": "2016年的搜狐内部台历设计，\\n以跑步为主题，\\n几次提案之后确定以图片+产品+文案的方式呈现。\\n过程就是先大量的搜图，\\n筛选既契合产品属性又有质感还能融合文案的图，\\n符合条件的图非常少，很多图都是好几张图拼合在一起的，\\n把每一张图都当做一张海报来做，\\n最后感觉效果还不错~ ：P",
+    "cover": "images/works/sohu-calendar/sohu-calendar-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/sohu-calendar/sohu-calendar-01.jpg",
+      "images/works/sohu-calendar/sohu-calendar-02.jpg",
+      "images/works/sohu-calendar/sohu-calendar-03.jpg",
+      "images/works/sohu-calendar/sohu-calendar-04.jpg",
+      "images/works/sohu-calendar/sohu-calendar-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "tencent-q2",
+    "title": "腾讯19Q2季度产品",
+    "views": 746,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "腾讯",
+      "衍生品",
+      "行政采购"
+    ],
+    "description": "",
+    "cover": "images/works/tencent-q2/tencent-q2-cover.jpg",
+    "coverW": 1280,
+    "coverH": 6459,
+    "detailImages": [
+      "images/works/tencent-q2/tencent-q2-01.jpg",
+      "images/works/tencent-q2/tencent-q2-02.jpg",
+      "images/works/tencent-q2/tencent-q2-03.jpg"
+    ],
+    "imageCount": 4
+  },
+  {
+    "slug": "vr-glasses",
+    "title": "VR眼镜盒包装",
+    "views": 773,
+    "tags": [
+      "原创Works",
+      "平面",
+      "包装",
+      "vr",
+      "VR眼镜"
+    ],
+    "description": "",
+    "cover": "images/works/vr-glasses/vr-glasses-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/vr-glasses/vr-glasses-01.jpg",
+      "images/works/vr-glasses/vr-glasses-02.jpg",
+      "images/works/vr-glasses/vr-glasses-03.jpg",
+      "images/works/vr-glasses/vr-glasses-04.jpg",
+      "images/works/vr-glasses/vr-glasses-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "wangzherongyao",
+    "title": "王者荣耀周边衍生",
+    "views": 785,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品"
+    ],
+    "description": "已参与 ：全心想象 寻找下一位王者 王者荣耀周边设计征集大赛\\n王者荣耀周边衍生",
+    "cover": "images/works/wangzherongyao/wangzherongyao-cover.jpg",
+    "coverW": 900,
+    "coverH": 650,
+    "detailImages": [
+      "images/works/wangzherongyao/wangzherongyao-01.jpg",
+      "images/works/wangzherongyao/wangzherongyao-02.jpg",
+      "images/works/wangzherongyao/wangzherongyao-03.jpg",
+      "images/works/wangzherongyao/wangzherongyao-04.jpg",
+      "images/works/wangzherongyao/wangzherongyao-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "weibo-h5",
+    "title": "那些年给微博做的H5",
+    "views": 735,
+    "tags": [
+      "原创Works",
+      "UI",
+      "交互/UE",
+      "微博",
+      "H5",
+      "财报"
+    ],
+    "description": "整理了近两年给微博做的H5页面\\n其中有一些已经上线的、没有链接的\\n还有一些没有上传的、以及一些飞机稿~",
+    "cover": "images/works/weibo-h5/weibo-h5-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/weibo-h5/weibo-h5-01.jpg",
+      "images/works/weibo-h5/weibo-h5-02.jpg",
+      "images/works/weibo-h5/weibo-h5-03.jpg",
+      "images/works/weibo-h5/weibo-h5-04.jpg",
+      "images/works/weibo-h5/weibo-h5-05.jpg"
+    ],
+    "imageCount": 6
+  },
+  {
+    "slug": "ziru-newyear",
+    "title": "自如2020年新年礼品",
+    "views": 2452,
+    "tags": [
+      "原创Works",
+      "工业/产品",
+      "生活用品",
+      "自如",
+      "新年",
+      "礼品"
+    ],
+    "description": "",
+    "cover": "images/works/ziru-newyear/ziru-newyear-cover.jpg",
+    "coverW": 1280,
+    "coverH": 720,
+    "detailImages": [
+      "images/works/ziru-newyear/ziru-newyear-01.jpg",
+      "images/works/ziru-newyear/ziru-newyear-02.jpg",
+      "images/works/ziru-newyear/ziru-newyear-03.jpg",
+      "images/works/ziru-newyear/ziru-newyear-04.jpg",
+      "images/works/ziru-newyear/ziru-newyear-05.jpg"
+    ],
+    "imageCount": 6
+  }
+];
